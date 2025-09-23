37eba456d38c6c7fef1a0ea61a0b97dc83a88ee3
croblespe <A25CandidoRP@iessanclemente.net>
2025-09-23 13:30:49 +0200

    exemplos tipo linguaxe

---
+++ ud01/HolaMundo.java
@@ -0,0 +1,5 @@
+public class HolaMundo { //No es correcto empezar el nombre de una clase en minusculas
+    public static void main(String[]  args) {
+        System.out.println("Hola Mundo!");
+    }
+}
---
+++ ud01/hola.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int i = 0;
+    for(i=0; i <= 1000; i++){
+	printf("hola mundo " );
+    }
+    return 0;
+}
